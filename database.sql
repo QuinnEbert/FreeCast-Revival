89bfb5e4aed0ed69c3e5bafbc108b08206d2de0d
Quinn Ebert <quinn@quinnebert.net>
2012-10-31 11:18:37 -0400

    Working insertion of registered user accounts to DB...
    It's a bloody miracle this still works…  PHP+MySQL code from 2004 should really be a lot more stubborn than this to get working.

---
+++ database.sql
@@ -0,0 +1,4 @@
+# Create a database called 'freecast' (or whatever) and import this SQL into it, then create a user and password to access the database fully,
+# and then edit the info in ./includes/db.php
+
+CREATE TABLE `user` (`name` TEXT NOT NULL , `nick` TEXT NOT NULL , `pass` TEXT NOT NULL , `email` TEXT NOT NULL , `show` TEXT NOT NULL , `showdesc` TEXT NOT NULL , `slots` TEXT NOT NULL)
--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,9 @@
 <?php
-require'../config.php';
+$dbhost = 'localhost';
+$dbuser = 'username';
+$dbpass = 'password';
+$dbname = 'freecast';
+
 $sql = mysql_connect($dbhost, $dbuser, $dbpass);
 mysql_select_db($dbname);
 ?>
