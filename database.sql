706c49ce5a09132180db82ae0f1eeaeffc5991ef
Quinn Ebert <quinn@quinnebert.net>
2012-11-07 11:18:15 -0500

    Add missing database column...
    The unique auto-increment "id" column somehow never got added to the SQL script.

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
 # Create a database called 'freecast' (or whatever) and import this SQL into it, then create a user and password to access the database fully,
 # and then edit the info in ./includes/db.php
 
-CREATE TABLE `user` (`name` TEXT NOT NULL , `nick` TEXT NOT NULL , `pass` TEXT NOT NULL , `email` TEXT NOT NULL , `show` TEXT NOT NULL , `showdesc` TEXT NOT NULL , `slots` TEXT NOT NULL, `allowemail` TEXT NOT NULL, `profilename` TEXT NOT NULL, `location` TEXT NOT NULL, `age` TEXT NOT NULL, `description` TEXT NOT NULL, `musicgenre` TEXT NOT NULL);
+CREATE TABLE `user` (`id` int(11) NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, `name` TEXT NOT NULL , `nick` TEXT NOT NULL , `pass` TEXT NOT NULL , `email` TEXT NOT NULL , `show` TEXT NOT NULL , `showdesc` TEXT NOT NULL , `slots` TEXT NOT NULL, `allowemail` TEXT NOT NULL, `profilename` TEXT NOT NULL, `location` TEXT NOT NULL, `age` TEXT NOT NULL, `description` TEXT NOT NULL, `musicgenre` TEXT NOT NULL);
